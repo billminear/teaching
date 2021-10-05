5b79d564ebb4894391a9a6e3975994ad7eac6c37
Bill Minear <wminear@wvup.edu>
2021-10-04 20:45:36 -0400

    added copying of the example lab contents into the directory of a newly created lab when one is created.

--- management/grade_configs/grade_configs.py
+++ management/grade_configs/grade_configs.py
@@ -2,6 +2,8 @@
 import os
 import sys
 import yaml
+
+from shutil import copytree
 from pathlib import Path
 
 try:
@@ -9,99 +11,102 @@ try:
 
 except IndexError:
     print("\nCreate new lab directory?")
-    new = input("(y/n) ")
+    create_new_directory = input("(y/n) ")
 
-    if new.lower() != "y":
+    if create_new_directory.lower() != "y":
         print("\nExiting.\n")
         exit()
 
-    lab_directory = input("\nLab directory name: ")
+    lab_directory_name = input("\nLab directory name: ")
     lab_directory_path = os.path.join(
         ".",
-        lab_directory,
+        lab_directory_name,
     )
-
     if not os.path.isdir(lab_directory_path):
-        os.mkdir(lab_directory_path)
-        print(f"{lab_directory_path} created.")
-        print("Modify files to your need.")
-        print("Run script again with lab directory as the first argument.")
-        print("Example: python grade_configs.py lab_directory\n")
-        exit()
+        new_path = copytree(
+            "example_lab_directory",
+            lab_directory_path,
+        )
+        configuration_file_path = os.path.join(new_path, "configuration_files")
+        for file in os.scandir(configuration_file_path):
+            os.remove(file.path)
 
-    else:
-        print(f"\n{lab_directory_path} already exists.")
-        print("Rerun the script with the directory as a the first argument.")
-        print("Example: python grade_configs.py lab_directory\n")
-        exit()
+#         print(f"{lab_directory_path} created.")
+#         print("Modify files to your need.")
+#         print("Run script again with lab directory as the first argument.")
+#         print("Example: python grade_configs.py lab_directory\n")
+#         exit()
 
+#     else:
+#         print(f"\n{lab_directory_path} already exists.")
+#         print("Rerun the script with the directory as a the first argument.")
+#         print("Example: python grade_configs.py lab_directory\n")
+#         exit()
 
-output_lines = "-" * 14 + "\n"
-output_lines += f"{lab_directory} grades\n"
-output_lines += "-" * 14
+# output_lines = "-" * 14 + "\n"
+# output_lines += f"{lab_directory} grades\n"
+# output_lines += "-" * 14
 
-files_to_skip = ["_required_statements.yml", "_grades.txt"]
-files_to_read = [
-    file.name for file in os.scandir(lab_directory_path) if file not in files_to_skip
-]
+# files_to_skip = ["_required_statements.yml", "_grades.txt"]
+# files_to_read = [
+#     file.name for file in os.scandir(lab_directory_path) if file not in files_to_skip
+# ]
 
-for filename in files_to_read:
-    student_file = os.path.join(lab_directory_path, filename)
+# for filename in files_to_read:
+#     student_file = os.path.join(lab_directory_path, filename)
 
-    with open(student_file, "r") as student_config_file:
-        student_config = student_config_file.readlines()
+#     with open(student_file, "r") as student_config_file:
+#         student_config = student_config_file.readlines()
 
-    lines_to_skip = ["!", ""]
-    student_config = [
-        line.strip() for line in student_config if line not in lines_to_skip
-    ]
-    student_config = set(student_config)
+#     lines_to_skip = ["!", ""]
+#     student_config = [
+#         line.strip() for line in student_config if line not in lines_to_skip
+#     ]
+#     student_config = set(student_config)
 
-    required_statements_yaml = os.path.join(
-        lab_directory_path, "_required_statements.yml"
-    )
-    with open(required_statements_yaml, "r") as required_statements_file:
-        required_statements = yaml.safe_load(required_statements_file)
+#     required_statements_yaml = os.path.join(
+#         lab_directory_path, "_required_statements.yml"
+#     )
+#     with open(required_statements_yaml, "r") as required_statements_file:
+#         required_statements = yaml.safe_load(required_statements_file)
 
-    statements = {
-        required_statement["statement"] for required_statement in required_statements
-    }
-    missing_statements = statements.difference(student_config)
+#     statements = {
+#         required_statement["statement"] for required_statement in required_statements
+#     }
+#     missing_statements = statements.difference(student_config)
 
-    # edge case for working around various delimeters in banner motds.
-    # may be a more appropriate way to handle this. works for now.
-    for line in student_config:
-        if "banner" in line:
-            missing_statements = [
-                missing_statement
-                for missing_statement in missing_statements
-                if "banner" not in missing_statement
-            ]
+#     # edge case for working around various delimeters in banner motds.
+#     # may be a more appropriate way to handle this. works for now.
+#     for line in student_config:
+#         if "banner" in line:
+#             missing_statements = [
+#                 if "banner" not in missing_statement
+#             ]
 
-    score = 0
-    for required_statement in required_statements:
+#     score = 0
+#     for required_statement in required_statements:
 
-        if required_statement["statement"] not in missing_statements:
-            score += required_statement["score"]
+#         if required_statement["statement"] not in missing_statements:
+#             score += required_statement["score"]
 
-    output_lines += "\n\n\n"
-    output_lines += f"{filename}\n"
-    output_lines += "-" * 14 + "\n"
+#     output_lines += "\n\n\n"
+#     output_lines += f"{filename}\n"
+#     output_lines += "-" * 14 + "\n"
 
-    for required_statement in required_statements:
-        statement = required_statement["statement"]
+#     for required_statement in required_statements:
+#         statement = required_statement["statement"]
 
-        if statement in missing_statements:
-            value = "-----"
-        else:
-            value = required_statement["score"]
-            value = f"{value:.2f}"
+#         if statement in missing_statements:
+#             value = "-----"
+#         else:
+#             value = required_statement["score"]
+#             value = f"{value:.2f}"
 
-        output_lines += f" {value.zfill(5):^} | {statement}\n"
+#         output_lines += f" {value.zfill(5):^} | {statement}\n"
 
-    output_lines += "-" * 14 + "\n"
-    output_lines += f" {score:^.2f} | Total\n"
+#     output_lines += "-" * 14 + "\n"
+#     output_lines += f" {score:^.2f} | Total\n"
 
-grade_file_path = os.path.join(lab_directory_path, "_grades.txt")
-with open(grade_file_path, "w+") as grade_file:
-    grade_file.write(output_lines)
+# grade_file_path = os.path.join(lab_directory_path, "_grades.txt")
+# with open(grade_file_path, "w+") as grade_file:
+#     grade_file.write(output_lines)
