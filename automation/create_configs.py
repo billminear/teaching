1d3625eaf6edb4bdc7014df4ba88508573583186
Bill Minear <wminear@wvup.edu>
2021-11-08 00:25:24 -0500

    script creates config directory if necessary and then writes configs to files now.

--- automation/create_configs.py
+++ automation/create_configs.py
@@ -1,14 +1,11 @@
+import os
 import yaml
 
 from jinja2 import Environment, FileSystemLoader
 
-# remove when complete.
-from pprint import pprint
-
 DEVICE_FILE = "devices.yaml"
 TEMPLATE_DIRECTORY = "./"
-# ROUTER_TEMPLATE = "router_template.j2"
-# SWITCH_TEMPLATE = "switch_template.j2"
+OUTPUT_DIRECTORY = "./configs"
 
 with open(DEVICE_FILE, "r") as device_filehandle:
     devices = yaml.safe_load(device_filehandle)
@@ -17,9 +14,16 @@ jinja_environment = Environment(
     loader=FileSystemLoader(TEMPLATE_DIRECTORY), trim_blocks=True, lstrip_blocks=True
 )
 
+if not os.path.isdir(OUTPUT_DIRECTORY):
+    os.makedirs(OUTPUT_DIRECTORY)
+
 for device in devices:
+
+    device_filename = f"{device['hostname']}.config"
+    output_file_path = os.path.join(OUTPUT_DIRECTORY, device_filename)
 
     jinja_template_name = f"{device['type']}.j2"
     jinja_template = jinja_environment.get_template(jinja_template_name)
 
-    print(jinja_template.render(device=device))
+    with open(output_file_path, "w") as output_file:
+        output_file.write(jinja_template.render(device=device))
